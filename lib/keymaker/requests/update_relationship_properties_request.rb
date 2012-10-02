e600c4e8bc0eee347ceec2c1f152e0eb696cc109
Martin Streicher <martin.streicher@gmail.com>
2012-10-01 22:04:11 -0400

    Work in progress.

---
+++ lib/keymaker/requests/update_relationship_properties_request.rb
@@ -0,0 +1,18 @@
+module Keymaker
+  class UpdateRelationshipPropertiesRequest < Request
+    def submit
+      service.put(node_properties_path(opts[:node_id]), node_properties).on_error do |response|
+        case response.status
+        when (400..499)
+          raise ClientError.new(response, response.body)
+        when (500..599)
+          raise ServerError.new(response, response.body)
+        end
+      end
+    end
+
+    def relationship_properties
+      opts.except(:node_id)
+    end
+  end
+end
